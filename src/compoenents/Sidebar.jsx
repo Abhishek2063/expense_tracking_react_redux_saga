f6a6eaefaf8708375d25e31d7142a9051619ae2a
abhishek <abhishekgarg2063@gmail.com>
2024-08-29 12:30:15 +0530

    added the validation in sidebar

--- src/compoenents/Sidebar.jsx
+++ src/compoenents/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Menu } from "antd";
+import React, { useEffect, useState } from "react";
+import { Menu, message } from "antd";
 import {
   DashboardOutlined,
   AppstoreOutlined,
@@ -10,41 +10,99 @@ import {
 } from "@ant-design/icons";
 import { Link, useLocation } from "react-router-dom";
 import "../assests/css/sidebar.css";
-import {
-  DASHBOARD_APP_URL,
-  MANAGE_CATEGORY_APP_URL,
-  MANAGE_EXPESENS_APP_URL,
-  MANAGE_MODULE_APP_URL,
-  MANAGE_ROLE_APP_URL,
-  MANAGE_USER_APP_URL,
-} from "../utils/app_route_list";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserDetails } from "../storage/user";
+import { getAllModule } from "../redux/module/module.action";
+import Loader from "./Loader";
+import _ from "lodash";
+import { usePrevious } from "../utils/custom_validation";
 
 const Sidebar = ({ isOpen }) => {
   const location = useLocation();
   const selectedKey = location.pathname;
+  const dispatch = useDispatch();
+  const userData = getUserDetails();
+  const [loader, setLoader] = useState(false);
+  const [moduleListData, setModuleListData] = useState([]);
+
+  const fetchAllModules = async (role_id) => {
+    setLoader(true);
+    dispatch(
+      getAllModule({
+        role_id: role_id,
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (userData) {
+      fetchAllModules(userData?.role?.id);
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  const getAllModuleData = useSelector(
+    (state) => state.module.getAllModuleData
+  );
+  const prevGetAllModuleData = usePrevious({ getAllModuleData });
+
+  useEffect(() => {
+    if (
+      prevGetAllModuleData &&
+      prevGetAllModuleData.getAllModuleData !== getAllModuleData
+    ) {
+      if (
+        getAllModuleData &&
+        _.has(getAllModuleData, "data") &&
+        getAllModuleData.success === true
+      ) {
+        message.success(getAllModuleData.message);
+        // Filter the modules based on permission
+        const modulesWithPermission = getAllModuleData?.data?.modules.filter(
+          (module) => module.has_permission
+        );
+        setModuleListData(modulesWithPermission);
+        setLoader(false);
+      }
+      if (getAllModuleData && getAllModuleData.success === false) {
+        setLoader(false);
+        message.error(getAllModuleData.message);
+      }
+    } // eslint-disable-next-line
+  }, [getAllModuleData, prevGetAllModuleData]);
+
+  // Define icon mapping for different routes
+  const iconMapping = {
+    "/dashboard": <DashboardOutlined />,
+    "/manage-module": <AppstoreOutlined />,
+    "/manage-role": <SettingOutlined />,
+    "/manage-user": <UserOutlined />,
+    "/manage-category": <FolderOpenOutlined />,
+    "/manage-expense": <DollarOutlined />,
+  };
 
   return (
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <Menu mode="inline" theme="dark" selectedKeys={[selectedKey]}>
-        <Menu.Item key={DASHBOARD_APP_URL} icon={<DashboardOutlined />}>
+        {/* Static menu item for Dashboard */}
+        {/* <Menu.Item
+          key={DASHBOARD_APP_URL}
+          icon={iconMapping[DASHBOARD_APP_URL]}
+        >
           <Link to={DASHBOARD_APP_URL}>Dashboard</Link>
-        </Menu.Item>
-        <Menu.Item key={MANAGE_MODULE_APP_URL} icon={<AppstoreOutlined />}>
-          <Link to={MANAGE_MODULE_APP_URL}>Manage Modules</Link>
-        </Menu.Item>
-        <Menu.Item key={MANAGE_ROLE_APP_URL} icon={<SettingOutlined />}>
-          <Link to={MANAGE_ROLE_APP_URL}>Manage Roles</Link>
-        </Menu.Item>
-        <Menu.Item key={MANAGE_USER_APP_URL} icon={<UserOutlined />}>
-          <Link to={MANAGE_USER_APP_URL}>Manage Users</Link>
-        </Menu.Item>
-        <Menu.Item key={MANAGE_CATEGORY_APP_URL} icon={<FolderOpenOutlined />}>
-          <Link to={MANAGE_CATEGORY_APP_URL}>Manage Category</Link>
-        </Menu.Item>
-        <Menu.Item key={MANAGE_EXPESENS_APP_URL} icon={<DollarOutlined />}>
-          <Link to={MANAGE_EXPESENS_APP_URL}>Manage Expenses</Link>
-        </Menu.Item>
+        </Menu.Item> */}
+
+        {/* Dynamic menu items based on module permissions */}
+        {moduleListData.map((module) => (
+          <Menu.Item
+            key={module.link_name}
+            icon={iconMapping[module.link_name]}
+          >
+            <Link to={module.link_name}>{module.name}</Link>
+          </Menu.Item>
+        ))}
       </Menu>
+      {loader && <Loader />}
     </div>
   );
 };
--- src/utils/Empty.jsx
+++ src/utils/Empty.jsx
@@ -8,10 +8,68 @@ import {
   MANAGE_USER_APP_URL,
   REGISTER_APP_URL,
 } from "./app_route_list";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserDetails } from "../storage/user";
+import { useEffect, useState } from "react";
+import { getAllModule } from "../redux/module/module.action";
+import Loader from "../compoenents/Loader";
+import { usePrevious } from "./custom_validation";
+import _ from "lodash";
+import { message } from "antd";
 
 const Empty = () => {
   const location = useLocation();
   const userLoggedIn = isLoggedIn();
+  const dispatch = useDispatch();
+  const userData = getUserDetails();
+  const [loader, setLoader] = useState(false);
+  const [moduleListData, setModuleListData] = useState([]);
+
+  const fetchAllModules = async (role_id) => {
+    setLoader(true);
+    dispatch(
+      getAllModule({
+        role_id: role_id,
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (userData) {
+      fetchAllModules(userData?.role?.id);
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  const getAllModuleData = useSelector(
+    (state) => state.module.getAllModuleData
+  );
+  const prevGetAllModuleData = usePrevious({ getAllModuleData });
+
+  useEffect(() => {
+    if (
+      prevGetAllModuleData &&
+      prevGetAllModuleData.getAllModuleData !== getAllModuleData
+    ) {
+      if (
+        getAllModuleData &&
+        _.has(getAllModuleData, "data") &&
+        getAllModuleData.success === true
+      ) {
+        message.success(getAllModuleData.message);
+        // Filter the modules based on permission
+        const modulesWithPermission = getAllModuleData?.data?.modules.filter(
+          (module) => module.has_permission
+        );
+        setModuleListData(modulesWithPermission);
+        setLoader(false);
+      }
+      if (getAllModuleData && getAllModuleData.success === false) {
+        setLoader(false);
+        message.error(getAllModuleData.message);
+      }
+    } // eslint-disable-next-line
+  }, [getAllModuleData, prevGetAllModuleData]);
 
   //   const path = URL.split("/");
   //   const id = path[2];
@@ -25,8 +83,19 @@ const Empty = () => {
     MANAGE_USER_APP_URL,
   ];
 
-  if (~matches.indexOf(location.pathname)) {
-    return <div></div>;
+  function checkLinkPermission(linkNameToCheck) {
+    if (moduleListData && moduleListData.length > 0) {
+      return moduleListData.some(
+        (obj) =>
+          obj.link_name === linkNameToCheck && obj.has_permission === true
+      );
+    } else {
+      return ~matches.indexOf(linkNameToCheck);
+    }
+  }
+
+  if (checkLinkPermission(location.pathname)) {
+    return <div> {loader && <Loader />}</div>;
   } else {
     return userLoggedIn ? (
       <Navigate to={DASHBOARD_APP_URL} />
